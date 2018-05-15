6468a42bb390127d51bf41da45a785858da6b0fd
yiphei <yanyiphei@gmail.com>
2018-05-15 11:18:49 -0400

    m

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // const ROOT_URL = 'https://cs52-blog.herokuapp.com/api';
-// const ROOT_URL = 'https://lab4cs52.herokuapp.com/api';
-const ROOT_URL = 'http://localhost:9090/api';
+const ROOT_URL = 'https://lab4cs52.herokuapp.com/api';
+// const ROOT_URL = 'http://localhost:9090/api';
 
 // // keys for actiontypes
 export const ActionTypes = {
